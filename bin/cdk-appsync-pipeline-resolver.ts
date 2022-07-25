0781ead788ebfed1f95b982420ab25115579e692
Michal Urbaniak <miurbani@amazon.ch>
2022-07-25 21:23:17 +0200

    Clean up main app file

--- bin/cdk-appsync-pipeline-resolver.ts
+++ bin/cdk-appsync-pipeline-resolver.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkApiGwStack } from '../lib/cdk-apigw-stack';
-import { CdkDynamoDbStack } from '../lib/cdk-dynamodb-stack';
 import { CdkAppsyncPipelineResolverStack } from '../lib/cdk-appsync-pipeline-resolver-stack';
 
 const app = new cdk.App();
 
 new CdkAppsyncPipelineResolverStack(app, 'CdkAppsyncPipelineResolverStack', {
-  env: { region: "us-east-1" }
+
 });
